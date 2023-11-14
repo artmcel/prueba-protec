a6a8e186367f65b5ce2ae75e065fc62a7f529e97
arturo <artmcel@hotmail.com>
2023-11-14 17:22:24 -0600

    se crea funcion index y retorna la vista

---
+++ app/Http/Controllers/VerEntradaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VerEntradaController extends Controller
+{
+    //
+
+    public function index(){
+        return view('ver');
+    }
+}
