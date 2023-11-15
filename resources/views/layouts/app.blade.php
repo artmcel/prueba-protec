b03923ca1f4667d97497262e3d07f7247fab8903
arturo <artmcel@hotmail.com>
2023-11-15 12:49:34 -0600

    add navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,9 +17,29 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
+    @if ( Route::current()->getName() !== 'home')
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ url('/') }}">Inicio</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link" href="{{ url('/crear-entradas')}}">Agrega entrada</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="{{ url('ver-entradas') }}">Ver entradas</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+    @endif
     <div id="app">
         <main class="py-4">            
-            @yield('inicio')
+            @yield('content')
         </main>
     </div>
 </body>
