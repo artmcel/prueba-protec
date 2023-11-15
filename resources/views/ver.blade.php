1468aa42329cadbf9ba0a37735170f736ff761b4
arturo <artmcel@hotmail.com>
2023-11-15 15:08:52 -0600

    fix tabulacion

--- resources/views/ver.blade.php
+++ resources/views/ver.blade.php
@@ -14,26 +14,26 @@
     </form>
 
     <div class="row">
-    @if ( session('resultado'))
-    <div class="alert alert-warning col-md-10 offset-md-1 text-center mt-3">
-        <p>Sin resultados</p>
-    </div>
-    @endif
-    @foreach($entradas as $entrada)
-    <div class="col m-2">
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h4 class="card-title">{{$entrada->titulo}}</h4>
-            <h6 class="card-subtitle text-muted">{{$entrada->autor}}</h6>
-            <p class="card-text">{{$entrada->contenido}}</p>
-            <a href="{{url('entrada/'.$entrada->id.'')}}" class="btn btn-primary">Ver mas...</a>
+        @if ( session('resultado'))
+        <div class="alert alert-warning col-md-10 offset-md-1 text-center mt-3">
+            <p>Sin resultados</p>
         </div>
-        <div class="card-footer">
-            <small class="text-muted">Fecha de Publicación: {{$entrada->fecha_publicacion}}</small>
+        @endif
+        @foreach($entradas as $entrada)
+        <div class="col m-2">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h4 class="card-title">{{$entrada->titulo}}</h4>
+                <h6 class="card-subtitle text-muted">{{$entrada->autor}}</h6>
+                <p class="card-text">{{$entrada->contenido}}</p>
+                <a href="{{url('entrada/'.$entrada->id.'')}}" class="btn btn-primary">Ver mas...</a>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Fecha de Publicación: {{$entrada->fecha_publicacion}}</small>
+            </div>
         </div>
+        </div>
+        @endforeach
     </div>
-    </div>
-    @endforeach
-</div>
 </div>
 @endsection
