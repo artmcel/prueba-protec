70538a88dcf6a2bffcd2e5b7ca090e74c7be4cdc
arturo <artmcel@hotmail.com>
2023-11-15 12:49:23 -0600

    se agrega form class

--- resources/views/crear.blade.php
+++ resources/views/crear.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
+
+@section('content')
 <div class="container">
     <h2>Agrega una entrada</h2>
-
     <div class="row">
         <div class="col">
             @if(session('status'))
@@ -11,43 +12,48 @@
             @endif
         </div>
     </div>
-    <form method="POST" action="{{url('/guarda-entrada')}}">
-        {{ csrf_field() }}
-        <div class="mb-3">
-          <label for="titulo" class="form-label">Titulo</label>
-          <input type="text"
-            class="form-control" name="titulo" id="titulo" aria-describedby="helpId" placeholder="Agrega el titulo">
-            @error('titulo')
-              <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-            @enderror
+    <div class="row justify-content-center align-items-center g-2">
+        <div class="col-md">
+            <form class="" method="POST" action="{{url('/guarda-entrada')}}">
+                {{ csrf_field() }}
+                <div class="mb-3">
+                  <label for="titulo" class="form-label">Titulo</label>
+                  <input type="text"
+                    class="form-control" name="titulo" id="titulo" aria-describedby="helpId" placeholder="Agrega el titulo">
+                    @error('titulo')
+                      <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                  <label for="autor" class="form-label">Autor</label>
+                  <input type="text"
+                    class="form-control" name="autor" id="autor" aria-describedby="helpId" placeholder="Agrega el Autor">
+                    @error('autor')
+                      <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                  <label for="fecha_publicacion" class="form-label">Fecha de Publicación</label>
+                  <input type="date"
+                    class="form-control" name="fecha_publicacion" id="fecha_publicacion" aria-describedby="helpId" placeholder="Agrega la fecha de publicación">
+                    @error('fecha_publicacion')
+                      <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                  <label for="contenido" class="form-label">Contenido</label>
+                  <textarea class="form-control" name="contenido" id="contenido" rows="3"></textarea>
+                  @error('contenido')
+                      <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                  @enderror
+                </div>
+                <div class="mb-3 row">
+                    <div class="mx-auto">
+                        <button type="submit" class="btn btn-primary">Guardar entrada</button>
+                    </div>
+                </div>
+            </form>
         </div>
-        <div class="mb-3">
-          <label for="autor" class="form-label">Autor</label>
-          <input type="text"
-            class="form-control" name="autor" id="autor" aria-describedby="helpId" placeholder="Agrega el Autor">
-            @error('autor')
-              <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="mb-3">
-          <label for="fecha_publicacion" class="form-label">Fecha de Publicación</label>
-          <input type="date"
-            class="form-control" name="fecha_publicacion" id="fecha_publicacion" aria-describedby="helpId" placeholder="Agrega la fecha de publicación">
-            @error('fecha_publicacion')
-              <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="mb-3">
-          <label for="contenido" class="form-label">Contenido</label>
-          <textarea class="form-control" name="contenido" id="contenido" rows="3"></textarea>
-          @error('contenido')
-              <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-          @enderror
-        </div>
-        <div class="mb-3 row">
-            <div class="offset-sm-4 col-sm-8">
-                <button type="submit" class="btn btn-primary">Guardar entrada</button>
-            </div>
-        </div>
-    </form>
+    </div>
 </div>
+@endsection
