428ed0c6bf0bb4079fa0b7aafa9c73a57371c121
arturo <artmcel@hotmail.com>
2023-11-14 21:12:20 -0600

    se agrega la funcion guardar

--- app/Http/Controllers/AltaEntradaController.php
+++ app/Http/Controllers/AltaEntradaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CreaEntradas;
 use Illuminate\Http\Request;
 
 class AltaEntradaController extends Controller
@@ -10,5 +11,28 @@ class AltaEntradaController extends Controller
 
     public function index(){
         return view('crear');
+    }
+
+    public function guardar( Request $request ){
+
+        $validaciones = $request->validate([
+            'titulo' => 'required',
+            'autor' => 'required',
+            'fecha_publicacion' => 'required',
+            'contenido' => 'required'
+        ]);
+
+        //print_r($request->all());
+
+        $crearEntrada = new CreaEntradas();
+        $crearEntrada->titulo = $request->titulo;
+        $crearEntrada->autor = $request->autor;
+        $crearEntrada->fecha_publicacion = $request->fecha_publicacion;
+        $crearEntrada->contenido = $request->contenido;
+        $crearEntrada->save();
+
+        return redirect('/crear-entradas')->with('status', 'Entrada guardada');
+
+
     }
 }
