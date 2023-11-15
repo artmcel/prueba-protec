0b7fd95c1b3321447b772339c6a52860c2268750
arturo <artmcel@hotmail.com>
2023-11-14 21:13:07 -0600

    se agrega la vista para la lista de entradas

---
+++ resources/views/ver.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+<div class="container">
+    <h2>listado de entradas</h2>
+
+    <div class="table-responsive">
+        <table class="table table-primary">
+            <thead>
+                <tr>
+                    <th scope="col">Titulo</th>
+                    <th scope="col">Autor</th>
+                    <th scope="col">Fecha de Publicación</th>
+                    <th scope="col">Contenido</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($entradas as $entrada)
+                <tr class="">
+                    <td scope="row">{{$entrada->titulo}}</td>
+                    <td>{{$entrada->autor}}</td>
+                    <td>{{$entrada->fecha_publicacion}}</td>
+                    <td>{{$entrada->contenido}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
